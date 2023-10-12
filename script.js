efc8df9682b6516f0d097bf0624f329c9791ea99
Aaron Jacob <aaronjacobaj4@gmail.com>
2023-10-12 23:53:03 +0530

    added theme-switch

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 const markdownInput = document.getElementById("markdow-input");
 const markdownPreview = document.getElementById("markdown-preview");
 
+function toggleTheme() {
+  const body = document.body;
+  const slider = document.getElementById('slider');
+
+  if (body.classList.contains('light-mode')) {
+      body.classList.remove('light-mode');
+      body.classList.add('dark-mode');
+      slider.classList.remove('active-sun');
+      slider.classList.add('active-moon');
+  } else {
+      body.classList.remove('dark-mode');
+      body.classList.add('light-mode');
+      slider.classList.remove('active-moon');
+      slider.classList.add('active-sun');
+  }
+}
+
 // Function to update the preview
 function updatePreview() {
   let inputText = markdownInput.value;
