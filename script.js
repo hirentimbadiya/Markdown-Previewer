1a2cb72db67635a799e38fe31fa9ffdf71791bce
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-10-15 11:08:38 +0530

    Toggle Fonts, dark mode

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 // Get the input and output elements
 const markdownInput = document.getElementById("markdow-input");
 const markdownPreview = document.getElementById("markdown-preview");
+const fontSelector = document.getElementById("font-family");
+const toggleSwitch = document.getElementById("toggle-switch");
+const body = document.body;
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the select element
+  const fontSelect = document.getElementById("font-family");
+
+  // Function to update font family based on selected option
+  function updateFontFamily() {
+      const selectedFont = fontSelect.value;
+
+      // Apply the selected font family to the entire document
+      document.body.style.fontFamily = selectedFont;
+
+      // Apply the selected font family to all text elements
+      const textElements = document.querySelectorAll("body *");
+      textElements.forEach(element => {
+          element.style.fontFamily = selectedFont;
+      });
+
+      const inputFields = document.querySelectorAll("input[type='text'], input[type='email'], textarea");
+      inputFields.forEach(input => {
+          input.style.fontFamily = selectedFont;
+      });
+  }
+
+  // Event listener for the select element change
+  fontSelect.addEventListener("change", updateFontFamily);
+});
+
+toggleSwitch.addEventListener("change", () => {
+  if (toggleSwitch.checked) {
+      body.classList.add("dark-mode");
+  } else {
+      body.classList.remove("dark-mode");
+  }
+});
+
 
 // Function to update the preview
 function updatePreview() {
